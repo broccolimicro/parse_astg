1c313952aa1b6d0c4bb231766075f8c9a14a8b48
Ned Bingham <edward.bingham@broccolimicro.io>
2026-08-01 15:17:53 -0400

    switching ~= to !=

--- parse_astg/expression.cpp
+++ parse_astg/expression.cpp
@@ -60,7 +60,7 @@ parse_expression::config makeExprConfig() {
 
 	cfg.order.push(operation_set::BINARY);
 	cfg.order.push_back("", "", "==", "");
-	cfg.order.push_back("", "", "~=", "");
+	cfg.order.push_back("", "", "!=", "");
 	cfg.order.push_back("", "", "<", "");
 	cfg.order.push_back("", "", ">", "");
 	cfg.order.push_back("", "", "<=", "");
